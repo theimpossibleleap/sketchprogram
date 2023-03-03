27768a8779b7e7a6860f76e581d6d9d8d07f0cd8
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-03-03 14:42:33 -0800

    Finished project. Can adjust moving forward.

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 const container = document.getElementById("container");
 const rowButton = document.getElementById('rowButt');
 
+function checkInput() {
+  let userInput = prompt(
+    "Please choose a number less than 150 for pixel size."
+  );
+
+  if (isNaN(userInput) || userInput > 150) return checkInput();
+
+  return userInput;
+};
+
+
+
 function makeDivs() {
-  let userInput = prompt("Choose a number of rows.");
-  let divRow = userInput;
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+
+  let canvasSize = checkInput();
+
+  let divRow = canvasSize;
   let divNum = divRow * divRow;
   let sqRoot = Math.round(Math.sqrt(divNum));
   let flex = (100 / sqRoot);
-  console.log(sqRoot);
 
   for (i = 0; i < divNum; i++) {
     let div = document.createElement('div');
-    div.className += "grid-item";
+    div.classList.add("grid-item");
     div.style.cssText = `
       height: ${flex}%;
       flex: 0 0 ${flex}%;
-      `
+      `;
+
+    div.addEventListener(
+      "mouseover",
+      (event) => {
+        div.classList.add("grid-item-change");
+      }
+    );
+
     container.appendChild(div);
   }
 };
