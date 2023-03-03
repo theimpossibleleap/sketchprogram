4e51eb538ee6b393148fcec023e79d5e4a7e6aff
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-03-03 12:11:36 -0800

    Changed files and got base running

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const container = document.getElementById("container");
+const rowButton = document.getElementById('rowButt');
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
+function makeDivs() {
+  let userInput = prompt("Choose a number of rows.");
+  let divRow = userInput;
+  let divNum = divRow * divRow;
+  let sqRoot = Math.round(Math.sqrt(divNum));
+  let flex = (100 / sqRoot);
+  console.log(sqRoot);
+
+  for (i = 0; i < divNum; i++) {
+    let div = document.createElement('div');
+    div.className += "grid-item";
+    div.style.cssText = `
+      height: ${flex}%;
+      flex: 0 0 ${flex}%;
+      `
+    container.appendChild(div);
+  }
 };
 
-makeRows(16, 16);
+rowButton.addEventListener('click', makeDivs);
