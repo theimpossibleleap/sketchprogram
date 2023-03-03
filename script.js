c57e4c4cb8ccc427a4f41d3ec6c91b78afe7e433
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-03-03 09:20:21 -0800

    Changed files

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
